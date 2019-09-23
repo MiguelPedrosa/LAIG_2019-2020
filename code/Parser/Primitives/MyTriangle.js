6dc952e22f5fc64d2ede33a185788cf8856256e3
MiguelHenriques <pedrosa.miguel@gmail.com>
2019-09-23 12:40:35 +0100

    Minor changes regarding documentation

--- code/Parser/Primitives/MyTriangle.js
+++ code/Parser/Primitives/MyTriangle.js
@@ -2,9 +2,9 @@
  * MyTriangle
  * @constructor
  * @param scene - Reference to MyScene object
- * @param dot1 - First dot compoused of a x, y and z variables
- * @param dot2 - Secound dot compoused of a x, y and z variables
- * @param dot3 - Third dot compoused of a x, y and z variables
+ * @param dot1 - First dot compoused of x, y and z variables
+ * @param dot2 - Secound dot compoused of x, y and z variables
+ * @param dot3 - Third dot compoused of x, y and z variables
  */
 class MyTriangle extends CGFobject {
 	constructor(scene, id, dot1, dot2, dot3) {
@@ -23,23 +23,31 @@ class MyTriangle extends CGFobject {
             this.dot3.x, this.dot3.y, this.dot3.z  // Vertex 2
         ];
 
-		//Counter-clockwise reference of vertices
+		/* Counter-clockwise reference of vertices */
 		this.indices = [
 			0, 1, 2
 		];
 
-		// Normals
-		let v1 = [this.dot2.x-this.dot1.x, this.dot2.y-this.dot1.y, this.dot2.z-this.dot1.z];
-		let v2 = [this.dot3.x-this.dot1.x, this.dot3.y-this.dot1.y, this.dot3.z-this.dot1.z];
+		/* Normals' calcutations*/
+		let vector1 = [
+			this.dot2.x - this.dot1.x, 
+			this.dot2.y - this.dot1.y,
+			this.dot2.z - this.dot1.z
+		];
+		let vector2 = [
+			this.dot3.x - this.dot1.x,
+			this.dot3.y - this.dot1.y,
+			this.dot3.z - this.dot1.z
+		];
 
-		let nx = v1[1]*v2[2] - v1[2]*v2[1];
-		let ny = v1[2]*v2[0] - v1[0]*v2[2];
-		let nz = v1[0]*v2[1] - v1[1]*v2[0];
+		let normalX = vector1[1] * vector2[2] - vector1[2] * vector2[1];
+		let normalY = vector1[2] * vector2[0] - vector1[0] * vector2[2];
+		let normalZ = vector1[0] * vector2[1] - vector1[1] * vector2[0];
 
 		this.normals = [
-			nx, ny, nz,
-			nx, ny, nz,
-			nx, ny, nz
+			normalX, normalY, normalZ,
+			normalX, normalY, normalZ,
+			normalX, normalY, normalZ
 		]
 		
 		// Texture coordenates
@@ -47,7 +55,8 @@ class MyTriangle extends CGFobject {
 			0, 1,
 			1, 1,
 			0.5, 0,
-		]
+		];
+
 		this.primitiveType = this.scene.gl.TRIANGLES;
 		this.initGLBuffers();
 	}
@@ -62,4 +71,3 @@ class MyTriangle extends CGFobject {
 		this.updateTexCoordsGLBuffers();
 	}
 }
-
