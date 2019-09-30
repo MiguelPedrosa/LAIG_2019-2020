1eddaa5889a1a6b5fe1244bc7805c47c0b254f7c
MiguelHenriques <pedrosa.miguel@gmail.com>
2019-09-30 20:45:44 +0100

    Added Torus

--- code/Parser/MySceneGraph.js
+++ code/Parser/MySceneGraph.js
@@ -674,7 +674,30 @@ class MySceneGraph {
                 var sphere = new MySphere(this.scene, primitiveId, radius, slices, stacks);
 
                 this.primitives[primitiveId] = sphere;
-            }  
+            } else if (primitiveType == 'torus') {
+                // inner radius
+                var inner = this.reader.getFloat(grandChildren[0], 'inner');
+                if (!(inner != null && !isNaN(inner)))
+                    return "unable to parse inner of the primitive coordinates for ID = " + primitiveId;
+
+                // outer radius
+                var outer = this.reader.getFloat(grandChildren[0], 'outer');
+                if (!(outer != null && !isNaN(outer)))
+                    return "unable to parse outer of the primitive coordinates for ID = " + primitiveId;
+
+                // slices
+                var slices = this.reader.getFloat(grandChildren[0], 'slices');
+                if (!(slices != null && !isNaN(slices)))
+                    return "unable to parse slices of the primitive coordinates for ID = " + primitiveId;
+
+                // loops
+                var loops = this.reader.getFloat(grandChildren[0], 'loops');
+                if (!(loops != null && !isNaN(loops)))
+                    return "unable to parse loops of the primitive coordinates for ID = " + primitiveId;
+
+                var torus = new MyTorus(this.scene, primitiveId, inner, outer, slices, loops);
+                this.primitives[primitiveId] = torus;
+            } 
             else {
                 console.warn("To do: Parse other primitives.");
             }
