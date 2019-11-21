87790bba9f1ffe2303f55d728369f7418feffd70
MiguelPedrosa <miguelPedrosa@gmail.com>
2019-11-21 21:01:16 +0000

    Added cylinder2 parsing

--- code/Parser/MySceneGraph.js
+++ code/Parser/MySceneGraph.js
@@ -1304,6 +1304,34 @@ class MySceneGraph {
 
                 var patch = new Patch(this.scene, primitiveId, nPointsU, nPointsV, npartsU, npartsV, points);
                 this.primitives[primitiveId] = patch;
+            }  else if (primitiveType == 'cylinder2') {
+                // slices
+                var slices = this.reader.getFloat(grandChildren[0], 'slices');
+                if (!(slices != null && !isNaN(slices)))
+                    return "unable to parse slices of the primitive coordinates for ID = " + primitiveId;
+
+                // stacks
+                var stacks = this.reader.getFloat(grandChildren[0], 'stacks');
+                if (!(stacks != null && !isNaN(stacks)))
+                    return "unable to parse stacks of the primitive coordinates for ID = " + primitiveId;
+
+                // top
+                var top = this.reader.getFloat(grandChildren[0], 'top');
+                if (!(top != null && !isNaN(top)))
+                    return "unable to parse top of the primitive coordinates for ID = " + primitiveId;
+
+                // base
+                var base = this.reader.getFloat(grandChildren[0], 'base');
+                if (!(base != null && !isNaN(base)))
+                    return "unable to parse base of the primitive coordinates for ID = " + primitiveId;
+
+                // height
+                var height = this.reader.getFloat(grandChildren[0], 'height');
+                if (!(height != null && !isNaN(height)))
+                    return "unable to parse height of the primitive coordinates for ID = " + primitiveId;
+                
+                let cylinder2 = new Cylinder2(this.scene, primitiveId, base, top, height, slices, stacks);
+                this.primitives[primitiveId] = cylinder2;
             } else {
                 console.warn("To do: Parse other primitives.");
             }
